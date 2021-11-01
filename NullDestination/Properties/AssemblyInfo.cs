028ae5af426c173801c64d4fa4feed59456d8344
Keith Martin <keith.martin@outlook.com.au>
2021-11-01 20:29:41 +1000

    Update AssembleyInfo.cs to support SQL 2019

--- NullDestination/Properties/AssemblyInfo.cs
+++ NullDestination/Properties/AssemblyInfo.cs
@@ -48,3 +48,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("14.0.0.0")]
 [assembly: AssemblyFileVersion("14.0.0.0")]
 #endif
+#if SQL2019
+[assembly: AssemblyVersion("15.0.0.0")]
+[assembly: AssemblyFileVersion("15.0.0.0")]
+#endif
